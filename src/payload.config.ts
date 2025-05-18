2cc108f1192ca048356192769ea25be3d2479505
Sabrina Sòlborg Soto <saso0001@stud.kea.dk>
2025-05-18 03:39:54 +0200

    .

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,7 +1,6 @@
 // storage-adapter-import-placeholder
 import { mongooseAdapter } from '@payloadcms/db-mongodb'
 import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
-// import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
 import { buildConfig } from 'payload'
@@ -34,12 +33,5 @@ export default buildConfig({
     },
   }),
   sharp,
-  plugins: [
-    payloadCloudPlugin(),
-    // vercelBlobStorage({
-    //   enabled: true,
-    //   token: process.env.BLOB_READ_WRITE_TOKEN,
-    //   collections: { media: true },
-    // }),
-  ],
+  plugins: [payloadCloudPlugin()],
 })
