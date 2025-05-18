46325c4179826ea87a742747b4ff8a90b6ff12cc
Sabrina Sòlborg Soto <saso0001@stud.kea.dk>
2025-05-18 04:09:50 +0200

    user config update

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -4,10 +4,45 @@ export const Users: CollectionConfig = {
   slug: 'users',
   admin: {
     useAsTitle: 'email',
+    defaultColumns: ['email', 'role'],
   },
   auth: true,
   fields: [
     // Email added by default
     // Add more fields as needed
+    {
+      type: 'row',
+      fields: [
+        {
+          type: 'text',
+          name: 'firstName',
+          label: 'First Name',
+        },
+        {
+          type: 'text',
+          name: 'lastName',
+          label: 'Last Name',
+        },
+      ],
+    },
+    {
+      type: 'select',
+      name: 'role',
+      options: [
+        {
+          label: 'Admin',
+          value: 'admin',
+        },
+        {
+          label: 'Editor',
+          value: 'editor',
+        },
+        {
+          label: 'Guest',
+          value: 'guest',
+        },
+      ],
+      defaultValue: 'editor',
+    },
   ],
 }
