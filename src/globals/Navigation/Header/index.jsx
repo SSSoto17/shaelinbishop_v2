59c7c17a1ef39c988d40e87520f1d473ecfa6d54
Sabrina Sòlborg Soto <saso0001@stud.kea.dk>
2025-05-26 01:15:35 +0200

    fix?

--- src/globals/Navigation/Header/index.jsx
+++ src/globals/Navigation/Header/index.jsx
@@ -1,9 +1,11 @@
+import Loading from '@/app/(frontend)/[[...slug]]/loading'
 import { payload } from '@/lib/utils'
 import Link from 'next/link'
 
 import { Hero } from '@/components'
 import AdminBar from '../../../globals/Navigation/AdminBar'
 
+import { Suspense } from 'react'
 import { FaInstagram, FaYoutube } from 'react-icons/fa6'
 
 export default function Header({ id, hero }) {
@@ -16,7 +18,7 @@ export default function Header({ id, hero }) {
         <AdminBar id={id} />
         <NavBar />
       </section>
-      {hero && <Hero {...hero} />}
+      <Suspense fallback={<Loading />}>{hero && <Hero {...hero} />}</Suspense>
     </header>
   )
 }
